00d0bf2aa9abd10c21e633860d9fdcd44bfdd86d
Dru Goradia <dru@atlasdigital.tv>
2015-09-15 17:35:55 -0700

    Adds windows platform paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
-default["ohai"]["plugin_path"]            = "/etc/chef/ohai_plugins"
+case node["platform_family"]
+when "windows"
+	default["ohai"]["plugin_path"] = "C:/chef/ohai_plugins"
+else
+	default["ohai"]["plugin_path"] = "/etc/chef/ohai_plugins"
+end
 default['ohai']['plugins']['ohai-plugin'] = 'plugins'
